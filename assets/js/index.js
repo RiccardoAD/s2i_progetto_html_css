f8eb7accdcf9c2be79074801c0ca1276420a2927
RiccardoAD <rick_95@libero.it>
2024-08-25 18:17:24 +0200

    add meta tags and success chamge button sending

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,25 +38,25 @@ function scrollActive(){
         }
     })
 }
-window.addEventListener('scroll', scrollActive)
+ window.addEventListener('scroll', scrollActive)
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+// /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
+     origin: 'top',
+     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
-});
+//      reset: true
+ });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+ sr.reveal('.home__social-icon',{ interval: 200}); 
+ sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 
 //  contact emailjs
-const contactForm= document.getElementById("contact-form");
-const submitBtn = document.querySelector("#submit-btn");
+const contactForm= document.querySelector("#contact-form");
+const submitBtn = document.querySelector(".submit-btn");
 const nameInput = document.querySelector("#user_name");
 const emailInput = document.querySelector("#user_email");
 const messageInput = document.querySelector("#message");
@@ -80,7 +80,7 @@ contactForm.addEventListener("submit", (e) => {
 
    emailjs.send(serviceID, templateID, inputFields)
    .then(() => {
-      submitBtn.innerText = "nessage sent successfully";
+      submitBtn.innerText = "message sent successfully";
       nameInput.value = "";
       emailInput.value = "";
       messageInput.value = ""; 
